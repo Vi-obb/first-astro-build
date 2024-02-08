e911acf76a72e5641138a8b8f990ed9fdda5d4ef
7heMagician <obbengvincent@gmail.com>
2024-02-08 15:14:40 +0000

    Added shadcnUI, tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vfo.vercel.app/',
-  integrations: [preact()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }
+  ), react()]
 });
